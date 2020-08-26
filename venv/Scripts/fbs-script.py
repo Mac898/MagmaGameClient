b1e36496306c79fb9d9cdb337ab2a92733cec0d7
MacLim <xmachd@gmail.com>
2020-08-26 21:48:35 +1000

    v0.1.1 fix some issues with fbs freeze

--- venv/Scripts/fbs-script.py
+++ venv/Scripts/fbs-script.py
@@ -1,6 +1,6 @@
 #!d:\code\magmagameclient\venv\scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'fbs==0.8.9','console_scripts','fbs'
-__requires__ = 'fbs==0.8.9'
+# EASY-INSTALL-ENTRY-SCRIPT: 'fbs==0.9.0','console_scripts','fbs'
+__requires__ = 'fbs==0.9.0'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('fbs==0.8.9', 'console_scripts', 'fbs')()
+        load_entry_point('fbs==0.9.0', 'console_scripts', 'fbs')()
     )
